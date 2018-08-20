77fa98698bfaacabc24579c690354ba66e930a09
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 04:23:25 +0000

    Completed Midnight_Count

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
-end
+def countdown(seconds_to_midnight)
+start = 0
+	  while seconds_to_midnight > start
+	    puts "#{seconds_to_midnight} SECOND(S)!"
+	    seconds_to_midnight -= 1
+	  end
+	  "HAPPY NEW YEAR!"
+	end
+
+  countdown(10)
+  
+ def countdown_with_sleep(seconds_to_midnight)
+   start = 0
+  #  pause = sleep(4)
+	  while seconds_to_midnight > start
+	    puts "#{seconds_to_midnight} SECOND(S)!"
+	    sleep(1)
+	    seconds_to_midnight -= 1
+      while seconds_to_midnight == seconds_to_midnight -1
+      sleep(4)
+      end 
+	  end
+	  "HAPPY NEW YEAR!"
+	end
+countdown_with_sleep(10)
