7a6023ff8744e0057188b6a93ef16ecc5aaea93f
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 04:08:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,17 @@ start = 0
 	end
 
   countdown(10)
+  
+  def _with_sleep(seconds_to_midnight)
+   start = 0
+   sleep = 2
+	  while seconds_to_midnight > start
+	    puts "#{seconds_to_midnight} SECOND(S)!"
+	    sleep
+	    seconds_to_midnight -= 1
+
+	  end
+	  "HAPPY NEW YEAR!"
+	end
+
+  countdown(10)
